600a73c9af3a600c52c3b43ebcbc118ee9a869e8
arturuqf <arturuqf@gmail.com>
2017-02-19 11:16:35 +0100

    Update MainActivity.java

--- app/src/main/java/projekt/artur/projektarturd/MainActivity.java
+++ app/src/main/java/projekt/artur/projektarturd/MainActivity.java
@@ -1,5 +1,6 @@
 package projekt.artur.projektarturd;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -33,25 +34,25 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btnClear) Button btnClear;
     @BindView(R.id.btnEqual) Button btnEqual;
 
-    @OnClick(R.id.btnOne) public void btnOne() { logic.number(btnOne, textView, textViewS); }
-    @OnClick(R.id.btnTwo) public void btnTwo() { logic.number(btnTwo, textView, textViewS); }
-    @OnClick(R.id.btnThree) public void btnThree() { logic.number(btnThree, textView, textViewS); }
-    @OnClick(R.id.btnFour) public void btnFour() { logic.number(btnFour, textView, textViewS); }
-    @OnClick(R.id.btnFive) public void btnFive() { logic.number(btnFive, textView, textViewS); }
-    @OnClick(R.id.btnSix) public void btnSix() { logic.number(btnSix, textView, textViewS); }
-    @OnClick(R.id.btnSeven) public void btnSeven() { logic.number(btnSeven, textView, textViewS); }
-    @OnClick(R.id.btnEight) public void btnEight() { logic.number(btnEight, textView, textViewS); }
-    @OnClick(R.id.btnNine) public void btnNine() { logic.number(btnNine, textView, textViewS); }
+    @OnClick(R.id.btnOne) public void btnOne() { logic.number(btnOne); }
+    @OnClick(R.id.btnTwo) public void btnTwo() { logic.number(btnTwo); }
+    @OnClick(R.id.btnThree) public void btnThree() { logic.number(btnThree); }
+    @OnClick(R.id.btnFour) public void btnFour() { logic.number(btnFour); }
+    @OnClick(R.id.btnFive) public void btnFive() { logic.number(btnFive); }
+    @OnClick(R.id.btnSix) public void btnSix() { logic.number(btnSix); }
+    @OnClick(R.id.btnSeven) public void btnSeven() { logic.number(btnSeven); }
+    @OnClick(R.id.btnEight) public void btnEight() { logic.number(btnEight); }
+    @OnClick(R.id.btnNine) public void btnNine() { logic.number(btnNine); }
 
-    @OnClick(R.id.btnPlus) public void btnPlus() { logic.body (btnPlus, 1, textView, textViewS, btnMinus); }
-    @OnClick(R.id.btnMinus) public void btnMinus() { logic.body (btnMinus, 2, textView, textViewS, btnMinus); }
-    @OnClick(R.id.btnMulti) public void btnMulti() { logic.body (btnMulti, 3, textView, textViewS, btnMinus); }
-    @OnClick(R.id.btnDivide) public void btnDivide() { logic.body (btnDivide, 4, textView, textViewS, btnMinus); }
+    @OnClick(R.id.btnPlus) public void btnPlus() { logic.body (btnPlus, 1); }
+    @OnClick(R.id.btnMinus) public void btnMinus() { logic.body (btnMinus, 2); }
+    @OnClick(R.id.btnMulti) public void btnMulti() { logic.body (btnMulti, 3); }
+    @OnClick(R.id.btnDivide) public void btnDivide() { logic.body (btnDivide, 4); }
 
-    @OnClick(R.id.btnZero) public void btnZero() { logic.zeroO(btnZero, textView, textViewS); }
-    @OnClick(R.id.btnClear) public void btnClear() { logic.clear(textView, textViewS); }
-    @OnClick(R.id.btnEqual) public void btnEqual() { logic.equalL(textView, textViewS); }
-    @OnClick(R.id.btnDot) public void btnDot() { logic.btnDotT(textView, textViewS); }
+    @OnClick(R.id.btnZero) public void btnZero() { logic.zeroO(); }
+    @OnClick(R.id.btnClear) public void btnClear() { logic.clear(); }
+    @OnClick(R.id.btnEqual) public void btnEqual() { logic.equalL(); }
+    @OnClick(R.id.btnDot) public void btnDot() { logic.btnDotT(); }
 
 
     @Override
@@ -62,6 +63,4 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-
 }
-
